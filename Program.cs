3b7ad56d27097182e3b23f50cd3c2ea396320499
Esteban Padilla <estebanpadilla469@gmail.com>
2025-02-05 11:49:32 -0600

    Se modifica clase variable

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Semantica_1
+{
+    public class Program : Token
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (Lenguaje lexico = new("prueba.cpp"))
+                {
+                    lexico.Programa();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
---
+++ prueba.cpp
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+static void Main(string[] args)
+{
+  //char e = 256;
+  int a = 1;
+  int b = 3;
+  int c = a + b;
+
+  if(c != 5) {
+    Console.WriteLine("La suma es distinta de 5");
+
+    if(a == 1) {
+      Console.WriteLine("La variable a es igual a 7" + a + b + c + "       ");
+    } else {
+      Console.WriteLine("Else ejecutado");
+    }
+  } else {
+    Console.WriteLine("La suma de a + b = " + c);
+  }
+  /*int b = Console.ReadLine();
+  int a = (3 + 5) * 8 - (10 - 2*b) / b; // 61
+  a--;
+  a+=40;
+  a*=2;
+  a--;
+  a-=99;
+
+  int n = 5;
+
+  for(b = 100; a < n; a++) {
+    b++;
+    while( b != 5 ) {
+      if(n == 5) {
+        Console.WriteLine("5 es igual a " + n + " " + a + " " + b );
+      } else {
+        Console.WriteLine("5 es diferente a " + n);
+      }
+    }
+  }
+
+  if(a % 2 != 0) {
+    Console.WriteLine("Es impar " + a);
+    if(b == 2) {
+      Console.WriteLine("b es igual a 2");
+    } else if( b > 3) {
+      Console.WriteLine("b es mayor a 3 y vale " + b);
+    }
+    else {
+      Console.WriteLine("b no es igual a 2 y vale " + b);
+    }
+  } else {
+    Console.WriteLine("Es impar");
+  }*/
+}
